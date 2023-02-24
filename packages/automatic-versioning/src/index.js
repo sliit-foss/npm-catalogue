d577fa8de956aa5ca3f1bade57c68914c19a52ae
Akalanka <akalankaperera128@gmail.com>
2023-02-24 21:57:20 +0530

    Fix: migrated to @colors/colors

--- packages/automatic-versioning/src/index.js
+++ packages/automatic-versioning/src/index.js
@@ -5,7 +5,7 @@ import path from "path";
 import defaultRunner from "./types/default";
 import tagBasedRunner from "./types/tag-based";
 
-require("colors");
+require("@colors/colors");
 
 const args = process.argv.slice(2);
 
