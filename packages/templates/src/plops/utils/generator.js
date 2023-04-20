cde7dd4ac3ae11786461ba38955ba2981d7e7070
Akalanka <akalankaperera128@gmail.com>
2023-04-20 14:52:58 +0530

    Patch: issue of .gitignore being renamed as .npmignore during pack

--- packages/templates/src/plops/utils/generator.js
+++ packages/templates/src/plops/utils/generator.js
@@ -26,6 +26,7 @@ const generator = (plop, templates) => {
             templateFiles: [
               `../stacks/${global.stack}/${path}/.*`,
               `../stacks/${global.stack}/${path}/.*/*`,
+              `../stacks/${global.stack}/${path}/**/.*`,
               `../stacks/${global.stack}/${path}/**`,
             ],
           },
