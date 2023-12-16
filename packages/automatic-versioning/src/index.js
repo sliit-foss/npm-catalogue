7ac681acb55fbb0059cb1b1bde556938bb618ed3
Akalanka <akalankaperera128@gmail.com>
2023-12-16 11:27:07 +0530

    Feat!: automatic-versioning - integrated commander.js
    Closes #108

--- packages/automatic-versioning/src/index.js
+++ packages/automatic-versioning/src/index.js
@@ -2,46 +2,59 @@
 
 /* eslint-disable no-console */
 
-import path from "path";
-
-import defaultRunner from "./types/default";
-import tagBasedRunner from "./types/tag-based";
+import { default as path } from "path";
+import { program, Option } from "commander";
+import { version } from "../package.json";
+import { defaultRunner, tagBasedRunner } from "./types";
 
 require("@colors/colors");
 
-const args = process.argv.slice(2);
+program
+  .name("automatic-versioning")
+  .description("CLI for automated commit based semantic versioning with excellent support for monorepositories")
+  .version(version);
+
+program
+  .addOption(
+    new Option("-n, --name <string>", "name of the library being versioned").default("@sliit-foss/automatic-versioning")
+  )
+  .option("-r, --root <string>", "root directory to use when executing the script")
+  .option("--skip-commit", "do not commit the incremented version")
+  .option("--tag-based", "run versioning based on git tags");
+
+[
+  new Option("--recursive", "recursively search for a matching commit prefix"),
+  new Option("--prerelease-tag <string>", "prerelease tag to use when running on a prerelease branch"),
+  new Option("--prerelease-branch <string>", "run prereleases on this branch"),
+  new Option(
+    "--ignore-prefixes <string>",
+    "comma separated list of commit prefixes to ignore when searching for a matching prefix"
+  )
+].forEach((option) => program.addOption(option.conflicts("tagBased")));
+
+const opts = program.parse().opts();
 
 const defaultRootDir = "../../../../";
 
-let prereleaseTag,
-  prereleaseBranch,
-  ignorePrefixes = [];
-let name = "@sliit-foss/automatic-versioning";
-let rootDir = defaultRootDir;
-let noCommitEdit = false,
-  noCommit = false,
-  recursive = false;
+opts.root ??= defaultRootDir;
+opts.ignorePrefixes = opts.ignorePrefixes?.split(",") ?? [];
 
-args.forEach((arg) => {
-  if (arg.includes("--name=")) name = arg.replace("--name=", "");
-  if (arg.includes("--rootDir=")) rootDir += arg.replace("--rootDir=", "");
-  if (arg.includes("--no-commit-edit")) noCommitEdit = true;
-  if (arg.includes("--no-commit")) noCommit = true;
-  if (arg.includes("--recursive")) recursive = true;
-  if (arg.includes("--prerelease-tag=")) prereleaseTag = arg.replace("--prerelease-tag=", "");
-  if (arg.includes("--prerelease-branch=")) prereleaseBranch = arg.replace("--prerelease-branch=", "");
-  if (arg.includes("--ignore-prefixes=")) ignorePrefixes = arg.replace("--ignore-prefixes=", "")?.split(",") ?? [];
-});
+console.log(`Running versioning script for ${opts.name}`.green);
 
-console.log(`Running version bump for ${name}`.green);
-
-if (rootDir !== defaultRootDir) {
-  const parentDir = path.resolve(__dirname, rootDir);
+if (opts.root !== defaultRootDir) {
+  const parentDir = path.resolve(__dirname, opts.root);
   process.chdir(parentDir);
 }
 
-if (args.includes("--tag-based")) {
-  tagBasedRunner(name, noCommit);
+if (opts.tagBased) {
+  tagBasedRunner(opts.name, opts.skipCommit);
 } else {
-  defaultRunner(name, noCommit, noCommitEdit, recursive, prereleaseTag, prereleaseBranch, ignorePrefixes);
+  defaultRunner(
+    opts.name,
+    opts.skipCommit,
+    opts.recursive,
+    opts.prereleaseTag,
+    opts.prereleaseBranch,
+    opts.ignorePrefixes
+  );
 }
--- scripts/bump-version.sh
+++ scripts/bump-version.sh
@@ -2,4 +2,4 @@ if [ "$TAG" = "latest" ]; then
   ignore=ci
 fi
 
-npx automatic-versioning --no-commit --recursive --prerelease-tag=blizzard --prerelease-branch=development --ignore-prefixes=$ignore  $1
+npx automatic-versioning --skip-commit --recursive --prerelease-tag=blizzard --prerelease-branch=development --ignore-prefixes=$ignore  $1
