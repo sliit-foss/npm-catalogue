dff0979a4df0241adebe39173f77e10af885baaa
Akalanka Perera <akalankaperera128@gmail.com>
2024-03-09 14:32:27 +0000

    Patch(functions): updated type declarations

--- packages/functions/types/async.d.ts
+++ packages/functions/types/async.d.ts
@@ -10,7 +10,21 @@ export function asyncHandler(fn: Function): (req: any, res: any, next: Function)
  */
 export function tracedAsyncHandler(fn: Function): (req: any, res: any, next: Function) => Promise<void>;
 
+/**
+ * @description Same as the `tracedAsyncHandler` but the log upon failure is a warning log
+ * @param fn The function to be invoked asynchronously
+ */
+export function fallibleAsyncHandler(fn: Function): (req: any, res: any, next: Function) => Promise<void>;
+
+/**
+ * @description A more stripped down version of asyncHandler without any logs
+ * @param fn The function to be invoked asynchronously
+ */
+export function plainAsyncHandler(fn: Function): (req: any, res: any, next: Function) => Promise<void>;
+
 export default {
   asyncHandler,
-  tracedAsyncHandler
+  tracedAsyncHandler,
+  fallibleAsyncHandler,
+  plainAsyncHandler
 };
