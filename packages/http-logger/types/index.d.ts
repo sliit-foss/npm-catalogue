4fbba319b1a9c877f4e3a4225deb834a781bf092
ThulinaWickramasinghe <thulinawickramasinghe@gmail.com>
2024-04-21 13:41:40 +0530

    Fix(http-logger): add proper type for loggable function

--- packages/http-logger/types/index.d.ts
+++ packages/http-logger/types/index.d.ts
@@ -1,24 +1,15 @@
 import { RequestHandler, Request } from "express";
 
 /**
- * @description A function to customize additional logging info
- * @param req The express request object
- * @returns additional data to be logged as key value pairs
- */
-interface LoggableFn {
-  (req: Request): Record<string, any>;
-}
-
-/**
  * @description Options for logging middleware function
  */
-interface HttpLoggerOptions {
+type HttpLoggerOptions = {
   whitelists?: string[];
-  loggable?: string[] | LoggableFn;
-}
+  loggable?: string[] | ((req: Request) => Record<string, any>[]);
+};
 
 /**
- * @description HTTP logging middleware function for Express.js
+ * @description Creates a HTTP logging middleware for Express.js
  * @param {HttpLoggerOptions} options Add options to customize logging
  * @returns {RequestHandler} A middleware function which will log http requests
  */
