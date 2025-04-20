eb72ab809d2352db0a138437defee6f4b8663f03
Akalanka Perera <akalankaperera128@gmail.com>
2025-04-20 19:39:19 +0530

    Build: updated build types script

--- scripts/build-types.sh
+++ scripts/build-types.sh
@@ -1,1 +1,1 @@
-npx tsc --declaration --emitDeclarationOnly --esModuleInterop --resolveJsonModule --outDir ./dist --rootDir ./src ./src/index.ts
+npx tsc --declaration --emitDeclarationOnly --esModuleInterop --resolveJsonModule --downlevelIteration --outDir ./dist --rootDir ./src ./src/index.ts
