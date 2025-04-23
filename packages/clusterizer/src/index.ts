fa1102faa701158d81545a71dcf438755ecfb92f
Akalanka Perera <akalankaperera128@gmail.com>
2025-04-23 21:52:43 +0530

    Feat!: clusterizer - propogated sigterm to workers

--- packages/clusterizer/src/index.ts
+++ packages/clusterizer/src/index.ts
@@ -4,33 +4,40 @@ import { ClusterizerOptions } from "../types";
 
 const cpuCount = cpus().length;
 
-const logPrefix = "Clusterizer -";
-
 const clusterize = async (
   app: (pid: number) => any,
-  { logger, workers, onMaster, onWorker, onWorkerExit }: ClusterizerOptions = {}
+  { logger = console, workers, onMaster, onWorker, onWorkerExit }: ClusterizerOptions = {}
 ) => {
-  if (!logger) logger = console;
+  const logPrefix = logger !== console ? "Clusterizer - " : "";
   try {
     workers = workers ?? cpuCount;
 
     if (cluster.isPrimary) {
       if (onMaster) onMaster();
-      else logger.info(`${logPrefix} Master ${process.pid} is running`);
+      else logger.info(`${logPrefix}Master ${process.pid} is running`);
 
       for (let i = 0; i < workers; i++) cluster.fork();
 
       cluster.on("exit", (worker, code, signal) => {
         if (onWorkerExit) onWorkerExit(worker, code, signal);
-        else logger.info(`${logPrefix} Worker ${worker.process.pid} died - code: ${code} - signal: ${signal}`);
+        else logger.info(`${logPrefix}Worker ${worker.process.pid} died - code: ${code} - signal: ${signal}`);
+      });
+
+      process.on("SIGTERM", () => {
+        for (const id in cluster.workers) {
+          const worker = cluster.workers[id];
+          if (worker && worker.process) {
+            worker.process.kill("SIGTERM");
+          }
+        }
       });
     } else {
       await app(process.pid);
       if (onWorker) onWorker();
-      else logger.info(`${logPrefix} Process ${process.pid} started`);
+      else logger.info(`${logPrefix}Process ${process.pid} started`);
     }
   } catch (e) {
-    logger.error(`${logPrefix} Unhandled exception - message: ${e.message} - stack: ${e.stack}`);
+    logger.error(`${logPrefix}Unhandled exception - message: ${e.message} - stack: ${e.stack}`);
   }
 };
 
